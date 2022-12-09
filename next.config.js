7e9955b9598098a4c32b6b6204492c1c7880d8ac
Anshuman <anshuman8swain@gmail.com>
2022-12-09 23:02:07 +0530

    feat:PWA added

--- next.config.js
+++ next.config.js
@@ -9,3 +9,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+module.exports = withPWA({
+	pwa: {
+		dest: "public",
+		register: true,
+        disable: process.env.NODE_ENV === 'development',
+		skipWaiting: true,
+	},
+});
