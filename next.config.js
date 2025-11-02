bcf8025f5496d82a6d21e489da5f4ce58e78da7d
anshuman <anshuman8swain@gmail.com>
2025-11-02 10:30:08 +0530

    fix:pkg update

--- next.config.js
+++ next.config.js
@@ -9,21 +9,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-
-
-// const withPWA = require('next-pwa')({
-//   dest: 'public'
-// })
-// module.exports = withPWA({
-// 	pwa: {
-// 		dest: "public",
-// 		register: true,
-//         disable: process.env.NODE_ENV === 'development',
-// 		skipWaiting: true,
-// 	},
-//   images: {
-//     domains: [],
-//     unoptimized: true
-//   }
-// });
